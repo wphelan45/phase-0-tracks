8149012d2f8363c9c9bb47234b7de7e8fdf0af61
Will Phelan <whenryph45@gmail.com>
2017-04-14 15:40:34 -0500

    9.4 Final

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -7,7 +7,7 @@ db.results_as_hash = true
 
 # write a basic GET route
 # add a query parameter
-# GET /
+# GET / -> Performs a get http call on a backslash. Always return a string
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
 end
@@ -23,6 +23,23 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+get '/contact' do
+  "123 Oriole Ave Cincinnatti, OH 45056"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if  name
+    "Good job, #{name}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/add/:num1/:num2' do
+  "#{params[:num1].to_i + params[:num2].to_i}"
+end
+
 # write a GET route that retrieves
 # all student data
 get '/students' do
@@ -32,7 +49,7 @@ get '/students' do
     response << "ID: #{student['id']}<br>"
     response << "Name: #{student['name']}<br>"
     response << "Age: #{student['age']}<br>"
-    response << "Campus: #{student['campus']}<br><br>"
+    response << "Campus: #{student['campus']}<br>"
   end
   response
 end
@@ -43,4 +60,27 @@ end
 get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
+end
+
+get '/students/:location' do
+  student_location = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])[2]
+  student_location.to_s
+end
+
+#Attempt to do optional release: Did not pass
+get '/students/parameter' do
+  parameter
+  if parameter=id
+    student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
+    student.to_s
+  elsif parameter=name
+    student = db.execute("SELECT * FROM students WHERE id=?", [params[:name]])[1]
+    student.to_s
+  elsif parameter=age
+    student = db.execute("SELECT * FROM students WHERE id=?", [params[:age]])[2]
+    student.to_s
+  elsif parameter=campus
+    student = db.execute("SELECT * FROM students WHERE id=?", [params[:campus]])[3]
+    student.to_s
+  end 
 end
